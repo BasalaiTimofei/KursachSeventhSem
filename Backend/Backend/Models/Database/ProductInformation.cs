993be4e3e70b684d7c25e499e9ae81f7f6bc9676
plagin_pain <003ovavos@gmail.com>
2019-12-04 05:31:25 +0300

    Add services

--- Backend/Backend/Models/Database/ProductInformation.cs
+++ Backend/Backend/Models/Database/ProductInformation.cs
@@ -4,7 +4,14 @@
     {
         public string Id { get; set; }
 
-        //Info
+        public string OperatingSystem { get; set; }
+        public float ScreenSize { get; set; }
+        public int Ram { get; set; }
+        public int Memory { get; set; }
+        /// <summary>
+        /// Количесто ядер
+        /// </summary>
+        public int NumberOfCores { get; set; }
 
         public string ProductId { get; set; }
         public Product Product { get; set; }
